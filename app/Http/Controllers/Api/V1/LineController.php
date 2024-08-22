ac4ea593b4971fc89ede7479714ab554bcaaf447
shun022406200726 <shun0224@MINISFORUM>
2024-08-22 12:18:26 +0900

    2024082201

--- app/Http/Controllers/Api/V1/LineController.php
+++ app/Http/Controllers/Api/V1/LineController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use LINE\LINEBot;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+
 class LineController extends Controller
 {
     // メッセージ送信
@@ -13,7 +17,11 @@ class LineController extends Controller
         // TODO: ここに具体的に実装
 
         // 1. 登録されている友だちにメッセージを送信
-
+        $httpClient = new CurlHTTPClient(env('LINE_CHANNEL_ACCESS_TOKEN'));
+        $bot = new LINEBot($httpClient, ['channelSecret' => env('CHANNEL_SECRET')]);
+        $messageBuilder=new TextMessageBuilder('test');
+        $bot->broadcast($messageBuilder);
+  
         return response()->json(['message' => 'sent']);
     }
 
