0072f9ce3be6a2cdcb85efb1c8696b469c4b9d7f
shun022406200726 <shun0224@MINISFORUM>
2024-08-24 07:12:05 +0900

    2024082402

--- app/Http/Controllers/Api/V1/LineController.php
+++ app/Http/Controllers/Api/V1/LineController.php
@@ -31,8 +31,39 @@ class LineController extends Controller
         // TODO: ここに具体的に実装
 
         // 1. 受け取った情報からメッセージの情報を取り出す
-        Log::debug($request->getContent());
+        // Log::debug($request->getContent());
+        $eventsObj = json_decode($request->getContent());
+        $replyToken='';
+        $replyMessage='';
+        if (is_null($eventsObj)|| is_null($eventsObj->events)) {
+            return response()->json(['message'=>'received(no events)']);
+        }
+
+        foreach ($eventsObj->events as $event) {
+            if ($event->type == 'message') {
+                $replyToken=$event->replyToken;
+                $message=$event->message;
+            }
+        }
+
         // 2. 受け取ったメッセージの内容から返信するメッセージを生成
+        switch ($message->text) {
+            case '今日の天気は？':
+                # code...
+                $replyMessage='はれ';
+                break;
+            case '元気？';
+                $replyMessage='元気';
+                break;           
+            default:
+                if (strops($message->text,'?')!==false) {
+                    # code...
+                    $replyMessage='質問は？'
+                }else{
+                    $replyMessage='は？'
+                }
+                break;
+        }
         // 3. 返信メッセージを返信先に送信
 
         return response()->json(['message' => 'received']);
